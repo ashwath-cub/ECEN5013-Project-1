9abb5bdb3d57ff6911b48e1a03bb9e4dab159300
“Ashwath <Ashwath.Gundepally@colorado.edu>
2016-09-17 23:37:51 -0600

    itoa's working fine

--- memory.c
+++ memory.c
@@ -54,21 +54,22 @@ uint8_t my_memzero(uint8_t* src, uint32_t length)
 
 uint8_t my_reverse(uint8_t *src, uint32_t length)
 {
-        if(length<0) //Failure. negative length makes no sense. Non-zero value is returned. Also tests for (length>2147483648)
-                return '1';
-
+  
         if(src==NULL) //Failure. The length and/or the address passed are incorrect.
-                return '2';
-
+                return '2'
         uint32_t index; //index to run the For loop below.
         uint8_t temp; //temp variable to facilitate exchange
-
+      
+       
         for(index=0;index<length/2;index++) //method to reverse the string; exchange elements //about the mid-point
         {
                 temp=*(src+index);
                 *(src+index)=*(src+length-(index+1));
                 *(src+length-(index+1))=temp;
         }
+        for(index=0;index<length; index++)
+            printf("%d\t",*(src+index));
+	printf("\nAddr: %x", src);
         return '0';
 }
 
--- memory.h
+++ memory.h
@@ -1,5 +1,5 @@
-#ifndef _HW1_H
-#define _HW1_H
+#ifndef prototype
+#define ptototype
 uint8_t my_memmove(uint8_t *src, uint8_t *dst, uint32_t length);
 uint8_t my_memzero(uint8_t *src, uint32_t length);
 uint8_t my_reverse(uint8_t *src, uint32_t length);
